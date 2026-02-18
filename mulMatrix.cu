04a2ba6b2566ba5e43f312fc4ec915b64fa916dd
envymoon <idang@ucdavis.edu>
2026-02-18 11:01:01 -0800

    Add files via upload

---
+++ mulMatrix.cu
@@ -0,0 +1,17 @@
+__global__ void matmul_naive(
+    const float* A,
+    const float* B,
+    float* C,
+    int M, int N, int K
+) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < M && col < N) {
+        float sum = 0.0f;
+        for (int k = 0; k < K; ++k) {
+            sum += A[row * K + k] * B[k * N + col];
+        }
+        C[row * N + col] = sum;
+    }
+}
