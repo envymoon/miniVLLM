5228b36e5eaf55a91b84c4f9422fc2b051658d72
Ian Dang <idang@ucdavis.edu>
2026-04-08 21:39:17 -0700

    updates

--- kernels/flash_attention.cu
+++ kernels/flash_attention.cu
@@ -2,46 +2,65 @@
 #include <helper_cuda.h>
 #include <helper_functions.h>
 #include <iostream>  
+#include <cuda_fp16.h>
 
 
-template <int block_size, int head_dim> __global__ void fused_kernel(
-    const int num_heads, 
-    const int* seq_len, 
+template <typename T>
+__device__ __forceinline__ T cast_to(float val);
+
+// float type conversion
+template <>
+__device__ __forceinline__ float cast_to<float>(float val) { return val; }
+
+// half type conversion
+template <>
+__device__ __forceinline__ __half cast_to<__half>(float val) { return __float2half(val); }
+ 
+
+template <typename T, int block_size, int head_dim> __global__ void fused_kernel(
+    // const int num_heads, 
+    const int max_seq_len, 
     const __restrict__ float* d_Q, 
     const __restrict__ float* d_K, 
     const __restrict__ float* d_V,
     __restrict__ float* d_Att
 ) {
-    __shared__ t_Q[block_size][head_dim];
-    __shared__ t_K[block_size][head_dim];
-    __shared__ t_V[block_size][head_dim];
+    __shared__ T t_Q[block_size][head_dim];
+    __shared__ T t_KV[block_size][head_dim];
+    //__shared__ float t_att[block_size][head_dim];
 
-    int counter = seq_lenlen / block_size;
+    int batch_idx = gridDim.x;
+    int head_idx = gridDim.y;
+    int block_idx = gridDim.z;
 
-    for (int i = 0; i < counter; i++) {
-        int q_x = 4 * blockIdx.x;
-        t_Q[q_x][] = 
-        t_Q[q_x + 1][] = 
-        t_Q[q_x + 2][] = 
-        t_Q[q_x + 3][] = 
+    float local_m = -INFINITY;
+    float local_l = 0.0f;
+
+    for (int i = 0; i < max_seq_len; i++) {
+        int q_x = blockIdx.x * 4;
+        int tx = threadIdx.x;
+        t_Q[q_x][tx] =  
+        t_Q[q_x + 1][tx] = 
+        t_Q[q_x + 2][tx] = 
+        t_Q[q_x + 3][tx] = 
+
 
     }
      
 }
 
 
-int attention_softmax_fused(
-    const int& total_token, 
+int attention_softmax_fused( 
     const int& block_size, 
     const int& batch_size, 
     const int& num_heads, 
-    const int* seq_len, 
+    const int& max_seq_len, 
     const int& head_dim
 ) {
 
     float *h_Q, *h_K, *h_V, h_Att;
-    
-    size_t sz = (size_t)batch_size * num_heads * cumu_seq_len * head_dim;
+
+    size_t sz += (size_t)batch_size * num_heads * max_seq_len * head_dim;
 
     cudaCheckErrors(cudaMallocHost(reinterpret_cast<void **>(&h_Q), sz));
     cudaCheckErrors(cudaMallocHost(reinterpret_cast<void **>(&h_K), sz));
@@ -67,18 +86,47 @@ int attention_softmax_fused(
     cudaCheckErrors(cudaMemcpyAsync(d_K, h_K, cudaMemcpyDeviceToHost, stream));
     cudaCheckErrors(cudaMemcpyAsync(d_V, h_V, cudaMemcpyDeviceToHost, stream));
 
-    dim3 block = (block_size / 4, 1, 1);    
-    dim3 grid = (batch_size, num_heads, 1);
+    checkCudaErrors(cudaStreamSynchronize(stream));
 
-    // Assuming that the input tensor dimension is [Batch_size, head_num, seq_len, head_dim]
-    switch (block_size) {
-        case 64: fused_kernel<32, head_dim><<<grid, block, 0, stream>>>(d_Q, d_K, d_V, d_Att); break;
-        case 128: fused_kernel<128, head_dim><<<grid, block, 0, stream>>>(d_Q, d_K, d_V, d_Att); break;
-        case 256: fused_kernel<256, head_dim><<<grid, block, 0, stream>>>(d_Q, d_K, d_V, d_Att); break;
-        default: fused_kernel<64, head_dim><<<grid, block, 0, stream>>>(d_Q, d_K, d_V, d_Att); break;
+    int block_serializing = block_size / 4
+    int num_tiles = (max_seq_len + block_size - 1) / (block_serializing);
+    dim3 block = (block_serializing, 1, 1);    
+    dim3 grid = (batch_size, num_heads, num_tiles);
+
+    cudaCheckErrors(cudaEventRecord);
+
+    // Requiring that the input tensor dimension is [Batch_size, head_num, seq_len, head_dim]
+    // each sequence should be paddded to same length
+    if (is_fp16) {
+        using T = __half;
+        switch (block_size) {
+            case 16: fused_kernel<32, head_dim><<<grid, block, 0, stream>>>(head_dim, d_Q, d_K, d_V, d_Att); 
+            break;
+            case 32: fused_kernel<32, head_dim><<<grid, block, 0, stream>>>(head_dim, d_Q, d_K, d_V, d_Att); 
+            break;
+            case 64: fused_kernel<64, head_dim><<<grid, block, 0, stream>>>(head_dim, d_Q, d_K, d_V, d_Att); 
+            break;
+            case 128: fused_kernel<128, head_dim><<<grid, block, 0, stream>>>(head_dim, d_Q, d_K, d_V, d_Att); 
+            break;
+            default: fused_kernel<16, head_dim><<<grid, block, 0, stream>>>(head_dim, d_Q, d_K, d_V, d_Att); 
+            break;
+        }
     }
-
-
+    else {
+        using T = float;
+        switch (block_size) {
+            case 16: fused_kernel<T, 32, head_dim><<<grid, block, 0, stream>>>(head_dim, d_Q, d_K, d_V, d_Att); 
+            break;
+            case 32: fused_kernel<T, 32, head_dim><<<grid, block, 0, stream>>>(head_dim, d_Q, d_K, d_V, d_Att); 
+            break;
+            case 64: fused_kernel<T, 64, head_dim><<<grid, block, 0, stream>>>(head_dim, d_Q, d_K, d_V, d_Att); 
+            break;
+            case 128: fused_kernel<T, 128, head_dim><<<grid, block, 0, stream>>>(head_dim, d_Q, d_K, d_V, d_Att); 
+            break;
+            default: fused_kernel<T, 16, head_dim><<<grid, block, 0, stream>>>(head_dim, d_Q, d_K, d_V, d_Att); 
+            break;
+        }
+    }
 
 
 }
